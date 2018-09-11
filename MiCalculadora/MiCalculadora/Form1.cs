9358ee9bd5c885e4552117653b85637014eb1608
gonzalo <greco_gonzalo@hotmail.com>
2018-09-11 17:20:03 -0300

    Esqueleto

---
+++ MiCalculadora/MiCalculadora/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiCalculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.MinimizeBox = false;
+            this.MaximizeBox = false;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            InitializeComponent();
+        }
+
+        private void BtnOperar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
