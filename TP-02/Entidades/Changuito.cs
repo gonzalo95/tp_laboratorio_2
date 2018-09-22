bca25e232374c79a841b23da246189fa98119ecc
gonzalo <greco_gonzalo@hotmail.com>
2018-09-21 21:59:25 -0300

    Tp 2

---
+++ TP-02/Entidades/Changuito.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    /// <summary>
+    /// No podrá tener clases heredadas.
+    /// </summary>
+    public sealed class Changuito
+    {
+        List<Producto> productos;
+        int espacioDisponible;
+        public enum ETipo
+        {
+            Dulce, Leche, Snacks, Todos
+        }
+
+        #region "Constructores"
+        private Changuito()
+        {
+            this.productos = new List<Producto>();
+        }
+        public Changuito(int espacioDisponible) : this()
+        {
+            this.espacioDisponible = espacioDisponible;
+        }
+        #endregion
+
+        #region "Sobrecargas"
+        /// <summary>
+        /// Muestro el Changuito y TODOS los Productos
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Changuito.Mostrar(this, ETipo.Todos);
+        }
+        #endregion
+
+        #region "Métodos"
+
+        /// <summary>
+        /// Expone los datos del elemento y su lista (incluidas sus herencias)
+        /// SOLO del tipo requerido
+        /// </summary>
+        /// <param name="c">Elemento a exponer</param>
+        /// <param name="ETipo">Tipos de ítems de la lista a mostrar</param>
+        /// <returns></returns>
+        public static string Mostrar(Changuito c, ETipo tipo)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("Tenemos {0} lugares ocupados de un total de {1} disponibles", c.productos.Count, c.espacioDisponible);
+            sb.AppendLine("");
+            foreach (Producto item in c.productos)
+            {
+                switch (tipo)
+                {
+                    case ETipo.Snacks:
+                        if(item is Snacks)
+                            sb.AppendLine(item.Mostrar());
+                        break;
+                    case ETipo.Dulce:
+                        if(item is Dulce)
+                            sb.AppendLine(item.Mostrar());
+                        break;
+                    case ETipo.Leche:
+                        if(item is Leche)
+                            sb.AppendLine(item.Mostrar());
+                        break;
+                    case ETipo.Todos:
+                        sb.AppendLine(item.Mostrar());
+                        break;
+                }
+            }
+            
+            return sb.ToString();
+        }
+        #endregion
+
+        #region "Operadores"
+        /// <summary>
+        /// Agregará un elemento a la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se agregará el elemento</param>
+        /// <param name="p">Objeto a agregar</param>
+        /// <returns></returns>
+        public static Changuito operator +(Changuito c, Producto p)
+        {
+            if (c.espacioDisponible > c.productos.Count)
+            {
+                foreach (Producto item in c.productos)
+                {
+                    if (item == p)
+                        return c;
+                }
+
+                c.productos.Add(p);
+            }
+
+            return c;
+        }
+        /// <summary>
+        /// Quitará un elemento de la lista
+        /// </summary>
+        /// <param name="c">Objeto donde se quitará el elemento</param>
+        /// <param name="p">Objeto a quitar</param>
+        /// <returns></returns>
+        public static Changuito operator -(Changuito c, Producto p)
+        {
+            foreach (Producto item in c.productos)
+            {
+                if (item == p)
+                {
+                    c.productos.Remove(item);
+                    break;
+                }
+            }
+
+            return c;
+        }
+        #endregion
+    }
+}
